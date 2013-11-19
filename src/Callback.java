14993417f5f693847ec5e25f3cb4231d3322eb70
nora <bla@blub.de>
2013-11-19 16:53:53 +0100

    added Callback class

---
+++ src/Callback.java
@@ -0,0 +1,22 @@
+import de.uniba.wiai.lspi.chord.data.ID;
+import de.uniba.wiai.lspi.chord.service.NotifyCallback;
+public class Callback implements NotifyCallback {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void retrieved(ID target) {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void broadcast(ID source, ID target, Boolean hit) {
+		// TODO Auto-generated method stub		
+	}
+
+}
